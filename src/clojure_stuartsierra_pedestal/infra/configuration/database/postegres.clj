1d52f338e8086fa0f55804ad64389db717064bf9
jaksonlima <jaksonsneider@gmail.com>
2025-01-22 22:23:41 -0300

    feat: added h2 in-memory

--- src/clojure_stuartsierra_pedestal/infra/configuration/database/postegres.clj
+++ src/clojure_stuartsierra_pedestal/infra/configuration/database/postegres.clj
@@ -1,13 +1,8 @@
 (ns clojure-stuartsierra-pedestal.infra.configuration.database.postegres
-  (:require [clojure-stuartsierra-pedestal.domain.url :as u]
-            [clojure.java.io :as io]
-            [clojure.tools.logging :as log]
+  (:require [clojure.tools.logging :as log]
             [com.stuartsierra.component :as component]
-            [hikari-cp.core :as hikari]
-            [next.jdbc :as jdbc]
-            [next.jdbc.sql :as sql])
-  (:use [clojure.pprint])
-  (:import (java.util UUID)))
+            [hikari-cp.core :as hikari])
+  (:use [clojure.pprint]))
 
 (defrecord Database [config]
   component/Lifecycle
@@ -22,82 +17,3 @@
 
 (defn new-database []
   (map->Database {}))
-
-(defn list-migration-files []
-  (->> (file-seq (io/file "resources/db/migration"))
-       (filter #(.isFile %))
-       (filter #(re-matches #"V1__.*\.sql" (.getName %)))
-       (sort-by #(.getName %))))
-
-(defn run-migrations [db]
-  (let [files (list-migration-files)]
-    (log/info "Found migrations:" (map #(.getName %) files))
-    (doseq [file files]
-      (let [sql (slurp file)] ; Lê o conteúdo do arquivo
-        (log/info "Executing migration:" (.getName file))
-        (jdbc/execute! db [sql]))))) ; Executa o SQL
-
-(defn setup-h2-aliases [db-spec]
-  (jdbc/execute! db-spec ["CREATE DOMAIN IF NOT EXISTS TIMESTAMPTZ AS TIMESTAMP"]))
-
-(defn db->url  [result]
-        (when result
-          (u/with (:url/id result)
-                  (:url/name result)
-                  (:url/origin result)
-                  (:url/hash result)
-                  (:url/active result)
-                  (.toInstant (:url/created_at result))
-                  (.toInstant (:url/updated_at result)))))
-
-(defn connection-h2 []
-  (let [database-config {:adapter           "h2"
-                         ;:url               "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;MODE=PostgreSQL;DB_CLOSE_ON_EXIT=FALSE"
-                         :url "jdbc:h2:mem:test;MODE=PostgreSQL;DATABASE_TO_UPPER=FALSE;DB_CLOSE_DELAY=-1"
-                         :username          "sa"
-                         :password          ""
-                         :maximum-pool-size 5}
-        connection (hikari/make-datasource database-config)]
-    (pprint connection)
-
-    (setup-h2-aliases connection)
-
-    (run-migrations connection)
-
-    (pprint connection)
-
-    (let [
-          data (sql/insert! connection :url
-                       {:id        (.toString (UUID/randomUUID))
-                        :name       "test"
-                        :origin     "http://local.com"
-                        :hash       "0a98em"})
-          result (sql/query connection ["select * from url as url"])
-
-          parse (db->url (first result))
-          ]
-
-      (pprint result)
-      (pprint (first result))
-      (pprint parse)
-      )
-    )
-  )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
